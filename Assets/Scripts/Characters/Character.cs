14daf2861ae7c4665aaaa1b9c63be4da9e7df060
Mathieu Bédu <mathieu.bedu@hotmail.fr>
2015-04-25 19:02:59 +0200

    Code refactoring

---
+++ Assets/Scripts/Characters/Character.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DiosesModernos {
+    public class Character : MonoBehaviour {
+        #region Properties
+        [Header ("General")]
+        [SerializeField]
+        [Tooltip ("Life points of the character")]
+        [Range (0.0f, 1000.0f)]
+        float _health = 100;
+        [SerializeField]
+        [Tooltip ("Speed of the character")]
+        [Range (0.0f, 100.0f)]
+        protected float _speed = 6;
+
+        [Header ("Links")]
+        [SerializeField]
+        [Tooltip ("Animation of the character (drag & drop the character model in child)")]
+        protected Animation _animation;
+        #endregion
+
+        #region Getters
+        /*public User user {
+            get { return _user; }
+        }*/
+        #endregion
+
+        #region API
+        public void TakeDamage (int damage) {
+            _health = Mathf.Clamp (_health, 0, _health - damage);
+            if (0 == _health) {
+                SoundManager.instance.StopMusic ();
+            }
+        }
+        #endregion
+
+        #region Unity
+        /*void Awake () {
+            _user = GetComponent<User> ();
+        }*/
+        #endregion
+
+        #region Private properties
+        //User _user;
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Characters/PlayableCharacter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DiosesModernos {
+    public class PlayableCharacter : Character {
+        #region Properties
+        [Header ("General")]
+        [SerializeField]
+        [Tooltip ("Character id")]
+        string _id;
+        #endregion
+
+        #region Getters
+        public string id {
+            get { return _id; }
+            set { _id = value; }
+        }
+        #endregion
+
+        #region Unity
+        void Awake () {
+            if (0 == _id.Length) {
+                _id = SystemInfo.deviceUniqueIdentifier;
+            }
+        }
+        #endregion
+    }
+}
